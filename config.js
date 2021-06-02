389acf57cf8e93a256a9e23c4799e73bb9162fa4
Sachin Rajbhandari <sachink.rb@gmail.com>
2021-06-01 19:51:20 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: "169831915075919",
+    clientSecret: "68b67dc5ec397790d74880f0dc11bc79",
+  },
 };
